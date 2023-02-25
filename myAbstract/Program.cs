ec3f9f514412e2bf3ea30a5a7673b2e5a437ebdd
michelle beckett <Pellerm@mail.nmc.edu>
2023-02-24 22:26:40 -0500

    Add project files.

---
+++ myAbstract/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Vegtables
+{
+    abstract class Vegtable
+    {
+        public abstract string Name { get; set;}
+        public abstract string Description();
+        public string IAm() 
+        { 
+            return "I'm a vegtable"; 
+        }
+    }
+    class Program
+    {
+        class Tomatoe : Vegtable
+        {
+            public override string Name { get; set; }
+            public string Type { get; set; }
+            public string Color { get; set; }
+
+            public Tomatoe()
+            {
+                Name = string.Empty;
+                Type = string.Empty;
+                Color = string.Empty;
+            }
+            public Tomatoe(string name, string type, string color)
+            {
+                Name = name;
+                Type = type;
+                Color = color;
+            }
+            public override string Description()
+            {
+                return "I am the color " + Color + "\nType: " + Type + "\nI am a " + Name;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Tomatoe broccoli = new Tomatoe();
+            broccoli.Name = "Broccoli";
+            broccoli.Type = "Bonarda";
+            broccoli.Color = "Purple";
+            Console.WriteLine(broccoli.IAm());
+            Console.WriteLine(broccoli.Description());
+        }
+    }
+}
